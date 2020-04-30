b32eba09d726bca056ce7b395e853ead0df455e5
minus3theta <yone_j_synthesis@yahoo.co.jp>
2020-04-30 20:36:19 +0900

    Implement rank1_with_table

--- src/bv.rs
+++ src/bv.rs
@@ -1,7 +1,8 @@
 use num_integer::Integer;
 use bitvec::prelude::{BitBox, Lsb0, BitStore};
 
-type Store = BitBox<Lsb0, u32>;
+type StoreUnit = u16;
+type Store = BitBox<Lsb0, StoreUnit>;
 
 #[derive(Eq, PartialEq, Ord, PartialOrd, Debug, Hash, Clone)]
 pub struct BV {
@@ -11,12 +12,12 @@ pub struct BV {
 }
 
 impl BV {
-  const LARGE: usize = 64 * 64;
-  const SMALL: usize = 32;
+  const SMALL: usize = StoreUnit::BITS as usize;
+  const LARGE: usize = 4 * Self::SMALL * Self::SMALL;
   const S_PER_L: usize = Self::LARGE / Self::SMALL;
 
   pub fn new(array: Store) -> Self {
-    let raw: &[<u32 as BitStore>::Access] = array.as_total_slice();
+    let raw: &[<StoreUnit as BitStore>::Access] = array.as_total_slice();
     let mut large_sum = vec![0; raw.len().div_ceil(&Self::S_PER_L)];
     let mut small_sum = vec![0; raw.len()];
     let mut current_l_sum = 0;
@@ -47,6 +48,26 @@ impl BV {
     self.large_sum[i / Self::LARGE] + self.small_sum[i / Self::SMALL] as u32 +
       (i / Self::SMALL * Self::SMALL .. i).filter(|&j| self.array[j]).count() as u32
   }
+
+  pub fn rank1_with_table(&self, i: usize, table: &Vec<Vec<u8>>) -> u32 {
+    let raw = self.array.as_total_slice()[i / Self::S_PER_L].load(core::sync::atomic::Ordering::Relaxed);
+    self.large_sum[i / Self::LARGE] + self.small_sum[i / Self::SMALL] as u32 +
+      table[raw as usize][i % Self::SMALL] as u32
+  }
+
+  pub fn rank_lookup_table() -> Vec<Vec<u8>> {
+    let mut table = vec![vec![0; Self::SMALL]; 1 << Self::SMALL];
+    for (value, row) in table.iter_mut().enumerate() {
+      let mut sum = 0;
+      for (pos, count) in row.iter_mut().enumerate() {
+        *count = sum;
+        if (value >> pos) & 1 == 1 {
+          sum += 1;
+        }
+      }
+    }
+    table
+  }
 }
 
 impl std::ops::Index<usize> for BV {
@@ -62,29 +83,36 @@ mod tests {
   use super::*;
   #[test]
   fn new_l_size_1() {
-    assert_eq!(BV::new(bitbox![Lsb0, u32; 0]).large_sum.len(), 1);
+    assert_eq!(BV::new(bitbox![Lsb0, u16; 0]).large_sum.len(), 1);
   }
   #[test]
   fn new_l_size_large() {
-    assert_eq!(BV::new(bitbox![Lsb0, u32; 0; BV::LARGE]).large_sum.len(), 1);
+    assert_eq!(BV::new(bitbox![Lsb0, u16; 0; BV::LARGE]).large_sum.len(), 1);
   }
   #[test]
   fn new_l_size_large_plus_1() {
-    assert_eq!(BV::new(bitbox![Lsb0, u32; 1; BV::LARGE + 1]).large_sum.len(), 2);
+    assert_eq!(BV::new(bitbox![Lsb0, u16; 1; BV::LARGE + 1]).large_sum.len(), 2);
   }
   #[test]
   fn new_large_sum() {
-    assert_eq!(BV::new(bitbox![Lsb0, u32; 1; BV::LARGE + 1]).large_sum, vec![0, 4096]);
+    assert_eq!(BV::new(bitbox![Lsb0, u16; 1; BV::LARGE + 1]).large_sum, vec![0, 1024]);
   }
   #[test]
   fn new_small_sum() {
-    let bv = BV::new((0 .. 50).map(|i| i < 20).collect::<BitVec<Lsb0, u32>>().into());
-    assert_eq!(bv.small_sum, vec![0, 20]);
+    let bv = BV::new((0 .. 50).map(|i| i < 20).collect::<BitVec<Lsb0, u16>>().into());
+    assert_eq!(bv.small_sum, vec![0, 16, 20, 20]);
   }
   #[test]
   fn rank() {
-    let bv = BV::new(bitbox![Lsb0, u32; 1; BV::LARGE + 1]);
+    let bv = BV::new(bitbox![Lsb0, u16; 1; BV::LARGE + 1]);
     assert_eq!(bv.rank1(10), 10);
     assert_eq!(bv.rank1(1000), 1000);
+  }
+  #[test]
+  fn rank_with_table() {
+    let bv = BV::new(bitbox![Lsb0, u16; 1; BV::LARGE + 1]);
+    let table = BV::rank_lookup_table();
+    assert_eq!(bv.rank1_with_table(10, &table), 10);
+    assert_eq!(bv.rank1_with_table(1000, &table), 1000);
   }
 }
